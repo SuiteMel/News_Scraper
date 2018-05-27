e935cb62c9e6fd16ef4356df1dc1bdc09eca80f3
Melissa Waltner <melissamwaltner@gmail.com>
2018-05-26 21:51:09 -0500

    Created general setup and attempted to get scraping to work

---
+++ server.js
@@ -0,0 +1,68 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var request = require("request");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/scrape", (req, res) => {
+  request("https://readwrite.com/", (error, response, html) => {
+    var $ = cheerio.load(html);
+
+    
+    $("div article").each((i, element) => {
+      var result = {};
+
+      var title = $(element).find("h2").text();
+      var summary = $(element).find("h2").find("a").attr("href");
+      var url = $(element).find("p").text();
+
+      db.Article.create(result)
+        .then((dbArticle) => {
+          console.log(dbArticle);
+        })
+        .catch((err) => {
+          return res.json(err);
+          //return err;
+        });
+    });
+
+    res.send("Scrape Complete");
+  });
+});
+
+app.get("/articles", (req, res) => {
+  db.Article.find({})
+    .then((dbArticle) => {
+      res.json(dbArticle);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+app.listen(PORT, function() {
+  console.log("App running on http:localhost:" + PORT);
+})
